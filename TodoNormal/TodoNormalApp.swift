6ed6f53a0ab649feb15bdee0d2781ec253033d68
Ticoxxo <ticoxxo@gmail.com>
2025-06-03 11:01:11 -0700

    Initial Commit

---
+++ TodoNormal/TodoNormalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoNormalApp.swift
+//  TodoNormal
+//
+//  Created by Alberto Almeida on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoNormalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoNormalTests/TodoNormalTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoNormalTests.swift
+//  TodoNormalTests
+//
+//  Created by Alberto Almeida on 03/06/25.
+//
+
+import Testing
+@testable import TodoNormal
+
+struct TodoNormalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
